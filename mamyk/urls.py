300770c09234859a84a39ee2366a81523e06c48b
Samyerkn <s_yerkin@kbtu.kz>
2026-07-30 05:20:50 +0500

    финальная версия фронтенда: карта, корзина, авторизация

--- mamyk/urls.py
+++ mamyk/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,3 +8,9 @@ urlpatterns = [
     path('api/', include('users_requests_donations.urls')),
     path('api/', include('shop.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
